d5e6580d24443a3e325b4692012bac85bbf4cbc3
Yinqi Zhao <yinqiz@usc.edu>
2022-02-21 21:42:00 -0500

    delete def_tune and def_control, add corresponding parameters into est.lucid function

--- R/lucid.R
+++ R/lucid.R
@@ -19,63 +19,51 @@
 #' \item{res_model}{a list stores LUCID model corresponding to each combination of tuning parameters}
 #' @export
 #'
-lucid <- function(G, Z, Y, CoG = NULL, CoY = NULL, family = "normal", useY = TRUE,
-                  K = 2:5, Rho_G = 0, Rho_Z_InvCov = 0, Rho_Z_CovMu = 0, CV = 5, tol = 0.001){
+lucid <- function(G, 
+                  Z, 
+                  Y, 
+                  CoG = NULL, 
+                  CoY = NULL, 
+                  family = "normal", 
+                  useY = TRUE,
+                  K = 2:5,
+                  Rho_G = 0, 
+                  Rho_Z_Mu = 0,
+                  Rho_Z_Cov = 0, 
+                  ...){
   # combinations of tuning parameters
-  tune_list <- expand.grid(K, Rho_G, Rho_Z_CovMu, Rho_Z_InvCov)
-  colnames(tune_list) <- c("K", "Rho_G", "Rho_Z_InvCov", "Rho_Z_CovMu")
+  tune_list <- expand.grid(K, Rho_G, Rho_Z_Mu, Rho_Z_Cov)
+  colnames(tune_list) <- c("K", "Rho_G", "Rho_Z_Mu", "Rho_Z_Cov")
   m <- nrow(tune_list)
   tune_list$BIC <- rep(0, m)
-  tune_list$CV <- rep(0, m)
   # fit models for each combination
-  res_model <- lapply(1:m, function(x){
-    K <- tune_list[x, 1]
-    Rho_G <- tune_list[x, 2]
-    Rho_Z_InvCov <- tune_list[x, 3]
-    Rho_Z_CovMu <- tune_list[x, 4]
-    selectG <- ifelse(Rho_G != 0, TRUE, FALSE)
-    selectZ <- ifelse(Rho_Z_CovMu == 0 && Rho_Z_InvCov == 0, FALSE, TRUE)
-    # cross validation
-    pred_error <- rep(0, CV)
-    index <- sample(rep(1:CV, length.out = nrow(G)))
-    for (i in 1:CV) {
-      fit <- try(est.lucid(G = as.matrix(G[index != i, ]), Z = Z[index != i, ], Y = as.matrix(Y[index != i, ]), 
-                           CoG = CoG[index != i, ], CoY = CoY[index != i, ],
-                           family = family, useY = useY, K = K,
-                           tune = def.tune(Rho_G = Rho_G,
-                                           Rho_Z_CovMu = Rho_Z_CovMu,
-                                           Rho_Z_InvCov = Rho_Z_InvCov,
-                                           Select_Z = selectZ,
-                                           Select_G = selectG),
-                           control = def.control(tol = tol)))
-      if("try-error" %in% class(fit)) {
-        pred_error[i] = NA
-      } else {
-        fit_pred = predict(fit, newG = as.matrix(G[index == i, ]), newZ = Z[index == i, ], 
-                           newCoG = CoG[index == i, ], newCoY = CoY[index == i, ], response = FALSE)
-        if(family == "normal") {
-          pred_error[i] = sum((as.vector(Y[index == i, ]) - fit_pred$pred.y)^2) # sum of residual squares
-        }
-        if(family == "binary") {
-          pred_error[i] = pROC::auc(as.vector(Y[index == i, ]), fit_pred$pred.y)
-          if(pred_error[i] < 0.5) {
-            pred_error[i] = 1 - pred_error[i]
-          }
-        }
-      }
+  res_model <- vector(mode = "list",
+                      length = m)
+  for(i in 1:m) {
+    fit <- try(est.lucid(G = G, 
+                         Z = Z, 
+                         Y = Y,
+                         CoG = CoG, 
+                         CoY = CoY,
+                         family = family, 
+                         useY = useY,
+                         K = tune_list[i, 1],
+                         Rho_G = tune_list[i, 2],
+                         Rho_Z_Mu = tune_list[i, 3],
+                         Rho_Z_Cov = tune_list[i, 4],
+                         ...))
+    if("try-error" %in% class(fit)) {
+      tune_list[i, 5] <- NA
+    } else {
+      tune_list[i, 5] <- summary(fit$BIC)
     }
-    tune_list$CV[x] = mean(pred_error, na.rm = TRUE)
-    return(fit)
-  })
-  # choose the best model
-  for (i in 1:m) {
-    fit <- res_model[[i]]
-    if(!("try-error" %in% class(fit))){
-      tune_list$BIC[i] <- summary(fit)$BIC
-    }
+    res_model[[i]] <- fit
   }
-  x <- min(tune_list$CV, na.rm = TRUE)
-  best_model <- res_model[[which(tune_list$CV == x)]]
+  x <- min(tune_list[, 5], na.rm = TRUE)
+  if(is.na(x)) {
+    stop("LUCID model fails to converge given current tuning parameters")
+  }
+  best_model <- res_model[[which(tune_list[, 5]== x)]]
   return(list(best_model = best_model,
               tune_list = tune_list,
               res_model = res_model))
