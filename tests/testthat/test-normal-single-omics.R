1e53a41acc8b728728332d562c15031ebcceff0f
Yinqi Zhao <yinqiz@usc.edu>
2022-02-28 11:44:59 -0500

    add more test in test-normal-single-omics.R

--- tests/testthat/test-normal-single-omics.R
+++ tests/testthat/test-normal-single-omics.R
@@ -1,3 +1,5 @@
+# LUCID - single omics, normal outcome
+
 
 test_that("check estimations of LUCID with normal outcome (K = 2)", {
   # run LUCID model
@@ -7,15 +9,15 @@ test_that("check estimations of LUCID with normal outcome (K = 2)", {
   cov <- sim_data$Covariate[1:200, ]
   X <- sim_data$X[1:200]
   i <- sample(1:2000, 1)
-  cat(paste("seed =", i))
-  fit1 <- est.lucid(G = G,
-                    Z = Z,
-                    Y = Y_normal,
-                    CoY = cov,
-                    family = "normal",
-                    K = 2,
-                    seed = i,
-                    modelName = "VVV")
+  cat(paste("test1 - seed =", i, "\n"))
+  invisible(capture.output(fit1 <- est.lucid(G = G,
+                                             Z = Z,
+                                             Y = Y_normal,
+                                             CoY = cov,
+                                             family = "normal",
+                                             K = 2,
+                                             seed = i,
+                                             modelName = "VVV")))
   pars <- fit1$pars
   beta_causal <- mean(pars$beta[2, 2:5])
   beta_non <- mean(pars$beta[2, 6:10])
@@ -33,4 +35,56 @@ test_that("check estimations of LUCID with normal outcome (K = 2)", {
   expect_equal(gamma_causal, 1, tolerance = 0.05)
   expect_equal(gamma_non, 0, tolerance = 0.05)
   expect_equal(sigma, 1, tolerance = 0.05)
+})
+
+
+test_that("check variable selection on G", {
+  # run LUCID model
+  G <- sim_data$G[1:200, ]
+  Z <- sim_data$Z[1:200, ]
+  Y_normal <- sim_data$Y_normal[1:200, ]
+  cov <- sim_data$Covariate[1:200, ]
+  X <- sim_data$X[1:200]
+  i <- sample(1:2000, 1)
+  cat(paste("test2 - seed =", i, "\n"))
+  invisible(capture.output(fit1 <- est.lucid(G = G,
+                                             Z = Z,
+                                             Y = Y_normal,
+                                             CoY = cov,
+                                             family = "normal",
+                                             K = 2,
+                                             seed = i,
+                                             modelName = "VVV",
+                                             Rho_G = 0.05)))
+  
+  # check parameters
+  expect_equal(class(fit1$select$selectG), "logical")
+  expect_equal(as.vector(fit1$select$selectG), 
+               c(rep(TRUE, 4), rep(FALSE, 6)))
+})
+
+
+test_that("check variable selection on Z", {
+  # run LUCID model
+  G <- sim_data$G[1:200, ]
+  Z <- sim_data$Z[1:200, ]
+  Y_normal <- sim_data$Y_normal[1:200, ]
+  cov <- sim_data$Covariate[1:200, ]
+  X <- sim_data$X[1:200]
+  i <- sample(1:2000, 1)
+  cat(paste("test3 - seed =", i, "\n"))
+  invisible(capture.output(fit1 <- est.lucid(G = G,
+                                             Z = Z,
+                                             Y = Y_normal,
+                                             CoY = cov,
+                                             family = "normal",
+                                             K = 2,
+                                             seed = i,
+                                             modelName = "VVV",
+                                             init_par = "random",
+                                             Rho_Z_Mu = 13,
+                                             Rho_Z_Cov = 0.05)))
+  
+  # check parameters
+  expect_equal(class(fit1$select$selectG), "logical")
 })
