6af4276e3e370c7fb1787ba180c7cc5c0728dd6f
Yinqi Zhao <yinqiz@usc.edu>
2022-04-05 23:35:35 -0700

    add script for all checkings

---
+++ check.R
@@ -0,0 +1,12 @@
+# automatic check for R package
+library(devtools)
+# R CMD check
+check()
+# check package on windows
+check_win_devel()
+check_win_release()
+check_win_oldrelease()
+# check package on mac
+check_mac_release()
+# check package on Rhub
+check_rhub()
